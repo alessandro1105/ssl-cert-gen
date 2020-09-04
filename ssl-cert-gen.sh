86a1057d79cb222edd3f4c0cb535b85f274259fe
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2020-09-04 19:22:00 +0200

    Added alias parameter

--- ssl-cert-gen.sh
+++ ssl-cert-gen.sh
@@ -1,8 +1,10 @@
 DEFAULT_CA_KEY=~/.dev/ca.key
 DEFAULT_CA_PEM=~/.dev/ca.pem
 
-CA_KEY=${2:-$DEFAULT_CA_KEY}
-CA_PEM=${3:-$DEFAULT_CA_PEM}
+FILENAME=${2:-$1}
+
+CA_KEY=${3:-$DEFAULT_CA_KEY}
+CA_PEM=${4:-$DEFAULT_CA_PEM}
 
 # Let's generate a CA key and cert
 # if they do not exists and add them
@@ -13,11 +15,11 @@ if [ ! -f $CA_KEY ]; then
     sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $CA_PEM
 fi
 
-openssl genrsa -out $1.key 2048
-openssl req -new -key $1.key -out $1.csr -subj /CN=$1
+openssl genrsa -out $FILENAME.key 2048
+openssl req -new -key $FILENAME.key -out $FILENAME.csr -subj /CN=$1
 
 
-cat > $1.ext << EOF
+cat > $FILENAME.ext << EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -27,4 +29,4 @@ DNS.1 = $1
 EOF
 
 # Now we generate the certificate and the key
-openssl x509 -req -in $1.csr -CA $CA_PEM -CAkey $CA_KEY -CAcreateserial -out $1.crt -days 825 -sha256 -extfile $1.ext
+openssl x509 -req -in $FILENAME.csr -CA $CA_PEM -CAkey $CA_KEY -CAcreateserial -out $FILENAME.crt -days 3650 -sha256 -extfile $FILENAME.ext
